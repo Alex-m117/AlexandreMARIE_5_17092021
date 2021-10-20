fe1d91593be3e4de8a62dab644165c653d8fc79d
Alexandre MARIE <Alex.m117@outlook.fr>
2021-10-20 16:14:03 +0200

    mise a jour js (save backup)

--- front-end/js/products.js
+++ front-end/js/products.js
@@ -1,22 +1,28 @@
 const urlProducts = "http://localhost:3000/api/cameras/${id}";
-const ProductID = new URL(location.href).searchParams.get('id');
-
+const ProductID = window.location.search;
+console.log(ProductID)
+const urlParams = new URLSearchParams(ProductID);
+console.log(urlParams)
+const id = urlParams.get("id");
+console.log(id)
 
 (async function productsPic() {
 
-	const item = await getProduct(ProductID);
-	console.log(item)
-})();
+	const item = await getProduct()
+
+})()
 
 
-function getProduct(ProductID){
+function getProduct(){
 	return fetch (urlProducts)
 		.then(function(response){
 			return response.json()
 		})
-		.then(function(camera){
-			return camera
+		.then(function(camID){
+			return camID 
+			console.log(camID)	
 		})
+
 		.catch(function(err){
 			alert (err)
 		})
@@ -24,21 +30,37 @@ function getProduct(ProductID){
 
 
 function displayProduct(item){
-	const templateElt = document.getElementById("tempproducts") 
-	const cloneElt = document.importNode(templateElt.content, true);
-
-	cloneElt.querySelector(".products__image").src = item.imageUrl;
-	cloneElt.querySelector(".products__h1").textContent = item.name;
-	cloneElt.querySelector(".products__price").textContent = item.price/100+ '€';
-	cloneElt.querySelector(".choix").valueContent = item.value;
-
-	document.querySelector("productsforms").appendChild(cloneElt)
-
+	
+	document.querySelector(".products__image").src = item.imageUrl;
+	document.querySelector(".products__h1").textContent = item.name;
+	document.querySelector(".products__price").textContent = item.price/100+ '€';
+	document.querySelector(".choix").valueContent = item.value;
 };
 
 
+function addBasket(item) {
+
+	let camProduct = {
+
+	nom: item.name,
+	id: item_id,
+	lentilles: item.lenses,
+	prix: item.price,
+	quantité: 1,
+};
 
 
+	document.querySelector(".products__panier").addEventListener('click', function(ajout) {
+			event.preventDefault()
+});
+
+
+
+let localAddProduct = JSON.parse(localStorage.getItem("item"));
+
+
+
+};
 
 
 
