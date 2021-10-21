ef081e902b1d477ee06726dab71b2f4f8d09509c
Alexandre MARIE <Alex.m117@outlook.fr>
2021-10-21 18:37:40 +0200

    mise à jour js avant mentorat

--- front-end/js/products.js
+++ front-end/js/products.js
@@ -1,8 +1,6 @@
 const urlProducts = "http://localhost:3000/api/cameras/${id}";
 const ProductID = window.location.search;
-console.log(ProductID)
 const urlParams = new URLSearchParams(ProductID);
-console.log(urlParams)
 const id = urlParams.get("id");
 console.log(id)
 
@@ -17,10 +15,10 @@ function getProduct(){
 	return fetch (urlProducts)
 		.then(function(response){
 			return response.json()
+			console.log(response)	
 		})
 		.then(function(camID){
 			return camID 
-			console.log(camID)	
 		})
 
 		.catch(function(err){
@@ -34,8 +32,14 @@ function displayProduct(item){
 	document.querySelector(".products__image").src = item.imageUrl;
 	document.querySelector(".products__h1").textContent = item.name;
 	document.querySelector(".products__price").textContent = item.price/100+ '€';
-	document.querySelector(".choix").valueContent = item.value;
+	for (let i = 0; i < item.lenses.length; i++)
+	document.querySelector(".choix").valueContent = item.value[i];
 };
+
+	const btnPanier = document.querySelector(".products__panier");
+	btnPanier.addEventListener("click", function(ajout) {
+	event.preventDefault()
+});
 
 
 function addBasket(item) {
@@ -44,19 +48,12 @@ function addBasket(item) {
 
 	nom: item.name,
 	id: item_id,
-	lentilles: item.lenses,
-	prix: item.price,
+	lentilles: item.value,
+	prix: item.price / 100,
 	quantité: 1,
 };
 
-
-	document.querySelector(".products__panier").addEventListener('click', function(ajout) {
-			event.preventDefault()
-});
-
-
-
-let localAddProduct = JSON.parse(localStorage.getItem("item"));
+let camProduct = JSON.parse(localStorage.getItem("item"));
 
 };
 
