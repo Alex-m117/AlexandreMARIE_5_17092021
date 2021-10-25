9a527058cff1c6e45c15707ec54494050c61e8da
Alexandre MARIE <Alex.m117@outlook.fr>
2021-10-25 21:05:57 +0200

    finalisation page produit et mise en place local storage

--- front-end/js/products.js
+++ front-end/js/products.js
@@ -1,21 +1,22 @@
-const urlProducts = "http://localhost:3000/api/cameras/${id}";
+
 const ProductID = window.location.search;
 const urlParams = new URLSearchParams(ProductID);
 const id = urlParams.get("id");
 console.log(id)
+const urlProducts = "http://localhost:3000/api/cameras/"+id;
 
 (async function productsPic() {
 
 	const item = await getProduct()
-
+	displayProduct(item)
+	addPanier(item)
 })()
 
 
 function getProduct(){
 	return fetch (urlProducts)
 		.then(function(response){
-			return response.json()
-			console.log(response)	
+			return response.json()	
 		})
 		.then(function(camID){
 			return camID 
@@ -30,45 +31,63 @@ function getProduct(){
 function displayProduct(item){
 	
 	document.querySelector(".products__image").src = item.imageUrl;
+	document.querySelector(".products__image").alt = item.name;
 	document.querySelector(".products__h1").textContent = item.name;
+	document.querySelector(".products__desc").textContent = item.description;
 	document.querySelector(".products__price").textContent = item.price/100+ '€';
-	for (let i = 0; i < item.lenses.length; i++)
-	document.querySelector(".choix").valueContent = item.value[i];
+
+	let lenseOption = "";
+		for (let i = 0; i < item.lenses.length; i++) {
+		lenseOption += "<option value='"+item.lenses[i]+"'>"+item.lenses[i]+"</option>";
+		
+	};
+	
+		document.querySelector("#choix_lenses").innerHTML=lenseOption;
+
 };
 
+
+function addPanier(item) {
+
 	const btnPanier = document.querySelector(".products__panier");
-	btnPanier.addEventListener("click", function(ajout) {
-	event.preventDefault()
-});
-
-
-function addBasket(item) {
+	
+	btnPanier.addEventListener("click", function(event) {
+	event.preventDefault();
+	
+	const clickLenses = document.querySelector("#choix_lenses");
+	const choixLenses = clickLenses.value;
 
 	let camProduct = {
 
 	nom: item.name,
-	id: item_id,
-	lentilles: item.value,
-	prix: item.price / 100,
+	id: item._id,
+	lentilles: choixLenses,
+	prix: item.price / 100 + "€",
 	quantité: 1,
-};
+}	
+	console.log(camProduct)
 
-let camProduct = JSON.parse(localStorage.getItem("item"));
+	let addLocalStorage = JSON.parse(localStorage.getItem("products"));
 
-};
+	console.log(addLocalStorage) 
 
-
-
-
-
-
-
-
-
+	if(addLocalStorage) {
+		addLocalStorage.push(camProduct);
+		localStorage.setItem("products", JSON.stringify(addLocalStorage));
+		console.log(addLocalStorage) 
+	}
 
 
+	else {
+		addLocalStorage = [];
+		addLocalStorage.push(camProduct);
+		localStorage.setItem("products", JSON.stringify(addLocalStorage));
+		console.log(addLocalStorage) 
+		}
 
-
+			
+}
+)};
 
 
 
