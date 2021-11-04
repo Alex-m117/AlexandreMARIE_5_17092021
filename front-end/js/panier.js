7530d2c5bf9fe10c4b622ddb3aff4a27dee92d6c
Alexandre MARIE <Alex.m117@outlook.fr>
2021-11-04 18:22:38 +0100

    Mise a jour des rejex et début du post & fetch pour la confirmation

--- front-end/js/panier.js
+++ front-end/js/panier.js
@@ -3,8 +3,7 @@ const templateElt = document.getElementById("temppanier");
 const panierElt = document.getElementById("panier__vide");
 
 (async function arrPanier(){
-	displayPanier()
-
+	displayPanier();
 })();
 
 function displayPanier(){
@@ -66,44 +65,181 @@ function displayPanier(){
 				let prixProductPanier = addLocalStorage[j].prix;
 				prixTotal.push(prixProductPanier);
 			}
-			//Mise en place de "reducer" (source MDN) pour l'addition des sommes du tableau prixTotal.
-			const reducer = (accumulator, currentValue) => accumulator + currentValue;
-			const price = prixTotal.reduce(reducer,0);
-			document.querySelector("#total").textContent = price + ',00 €';
-		};
-	};			
+		//Mise en place de "reducer" (source MDN) pour l'addition des sommes du tableau prixTotal.
+		const reducer = (accumulator, currentValue) => accumulator + currentValue;
+		const price = prixTotal.reduce(reducer,0);
+		document.querySelector("#total").textContent = price + ',00 €';
+	};
+};
 
-// Formulaire --
+// Formulaire
+let formulaire = document.querySelector(".formulaire");
 
-// Ecoute de l'envoi du formulaire (conditions)
+// Création des fontions d'écoutes qui appèlent l'input pour les conditions rejex.
+formulaire.nom.addEventListener('change', function() {
+	validNom(this);
+});
 
-document.getElementById("commander").addEventListener('click',() => {
-	// Mise en place Rejex via javascript avant création du tableau contact.
-	let infoNom = document.getElementById("nom").value;
-	let infoPrenom = document.getElementById("prenom").value;
-	let infoMail = document.getElementById("mail").value;
-	let infoAdresse = document.getElementById("adresse").value;
-	let infoVille = document.getElementById("ville").value;
-})
-	const regexNomPrenom = new RegExp ("^[A-Za-z]{1,30}$");
+formulaire.prenom.addEventListener('change', function() {
+	validPrenom(this);
+});
+
+formulaire.mail.addEventListener('change', function() {
+	validMail(this);
+});
+
+formulaire.adresse.addEventListener('change', function() {
+	validAdresse(this);
+});
+
+formulaire.ville.addEventListener('change', function() {
+	validVille(this);
+});
+
+// Contrôle Rejex de l'input "Nom"
+const validNom = function(inputNom) {
+	const regexNomPrenom = new RegExp ("^[A-Za-z ]{1,30}$");
+
+	// Récupération de la balise SMALL
+	let small = inputNom.nextElementSibling;
+
+	if (regexNomPrenom.test(inputNom.value)) {
+		small.innerHTML = "Nom Valide";
+		small.classList.remove("text-danger");
+		small.classList.add("text-success");
+	}
+	else {
+		small.innerHTML = "Erreur de Saisie";
+		small.classList.remove("text-success");
+		small.classList.add("text-danger");
+	}
+};
+
+// Contrôle Rejex de l'input "Prénom"
+const validPrenom = function(inputPrenom) {
+	const regexNomPrenom = new RegExp ("^[A-Za-z ]{1,30}$");
+
+	// Récupération de la balise SMALL
+	let small = inputPrenom.nextElementSibling;
+
+	if (regexNomPrenom.test(inputPrenom.value)) {
+		small.innerHTML = "Prénom Valide";
+		small.classList.remove("text-danger");
+		small.classList.add("text-success");	
+	}
+	else {
+		small.innerHTML = "Erreur de Saisie";
+		small.classList.remove("text-success");
+		small.classList.add("text-danger");
+	}
+};
+
+// Contrôle Rejex de l'input "E-mail"
+const validMail = function(inputMail) {
 	const regexEmail = new RegExp ("^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([_\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$");
+
+	// Récupération de la balise SMALL
+	let small = inputMail.nextElementSibling;
+
+	if (regexEmail.test(inputMail.value)) {
+		small.innerHTML = "E-mail Valide";
+		small.classList.remove("text-danger");
+		small.classList.add("text-success");
+	}
+	else {
+		small.innerHTML = "Erreur de Saisie";
+		small.classList.remove("text-success");
+		small.classList.add("text-danger");
+	}
+};
+
+// Contrôle Rejex de l'input "Prénom"
+const validAdresse = function(inputAdresse) {
 	const regexAdresse = new RegExp ("^[A-Za-z0-9, _-]{3,60}$");
-	const regexVille = new RegExp ("^[A-Za-z-]{1,30}$");
 
-//if {
+	// Récupération de la balise SMALL
+	let small = inputAdresse.nextElementSibling;
 
+	if (regexAdresse.test(inputAdresse.value)) {
+		small.innerHTML = "Adresse Valide";
+		small.classList.remove("text-danger");
+		small.classList.add("text-success");	
+	}
+	else {
+		small.innerHTML = "Erreur de Saisie";
+		small.classList.remove("text-success");
+		small.classList.add("text-danger");
+	}
+};
 
-//}
-	
+// Contrôle Rejex de l'input "Prénom"
+const validVille = function(inputVille) {
+	const regexVille = new RegExp ("^[A-Za-z- ]{1,30}$");
 
-// Création fiche contact.
-// let contact = {
-//	firstname: document.getElementById("nom").value,
-//	lastname: document.getElementById("prenom").value,
-//	email: document.getElementById("mail").value,
-//	adress: document.getElementById("adresse").value,
-//	city: document.getElementById("ville").value,
-//};
+	// Récupération de la balise SMALL
+	let small = inputVille.nextElementSibling;
+
+	if (regexVille.test(inputVille.value)) {
+		small.innerHTML = "Ville Valide";
+		small.classList.remove("text-danger");
+		small.classList.add("text-success");	
+	}
+	else {
+		small.innerHTML = "Erreur de Saisie";
+		small.classList.remove("text-success");
+		small.classList.add("text-danger");
+	}
+};
+
+// Ecoute de l'envoi du formulaire 
+document.getElementById("commander").addEventListener('click',(event) => {
+	event.preventDefault();
+
+	if (validNom(formulaire.nom) 
+		&& validPrenom(formulaire.prenom) 
+		&& validMail(formulaire.mail) 
+		&& validAdresse(formulaire.adresse) 
+		&& validVille(formulaire.ville)) {
+
+		// Création fiche contact
+	let contact = {
+
+		firstname: formulaire.nom.value,
+		lastname: formulaire.prenom.value,
+		adress: formulaire.adresse.value,
+		city: formulaire.ville.value,
+		email: formulaire.mail.value,
+	}
+
+		//Récupération des ID produits pour les envoyer au back-end via la requête POST.
+		let productsID = [];
+
+		for(let i =0; i < addLocalStorage.length; i++) {
+			productsID.push(addLocalStorage[i].id)
+		};
+
+		// Création requête "POST"
+		const post = { 	
+			method: "POST",
+			headers: { 
+				'Accept': 'application/json', 
+				'Content-Type': 'application/json' 
+			},
+			body: JSON.stringify({contact, productID})
+		};
+
+		fetch("http://localhost:3000/api/cameras/order", post)
+		.then(response => response.json())
+		.then(response => { 
+			let commande = JSON.stringify(response)
+			localStorage.setItem("order", commande)
+			console.log(response)
+		})
+		.catch(err => {
+			alert('Envoi des données impossible, merci de vérifier vos informations.')
+		})
+	};
+});
 
 
 
